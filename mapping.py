bfd136d03bc79637467047b53b932e7390b41524
YoannDEWILDE <yoann.dewilde@etudiant.univ-rennes1.fr>
2022-11-21 11:29:57 +0100

    Fix seed-and-extend to support revcomp

--- mapping.py
+++ mapping.py
@@ -17,9 +17,14 @@ class MappingBest(Mapping):
         self.R = bwt.get_R(self.bwt)
     
     def find_with_bwt(self, pattern):
+        """Search pattern in reference"""
         return bwt.P_in_S(pattern, self.bwt, self.N, self.R, self.sa)
 
     def find_seeds_for_one_read(self, read):
+        """Return seeds of size self.k which are in reference
+        
+        Return: (position of the seed in read, position of the seed in ref)
+        """
         res = []
 
         for i in range(len(read) - self.k):
@@ -33,6 +38,12 @@ class MappingBest(Mapping):
         return res
 
     def extend(self, read, positions_pair):
+        """Extend of the seed.
+        
+        Params: positions_pair: result of find_seeds_for_one_read
+        
+        Return: (position in ref, read content)
+        """
         (pos_in_read, pos_in_ref) = positions_pair
         pos_ref_to_cmp = pos_in_ref - pos_in_read
 
@@ -44,31 +55,55 @@ class MappingBest(Mapping):
 
         return (pos_ref_to_cmp, utils.score(ref_to_cmp, read))
 
+    def align(self, w):
+        """Seed-and-extend on a specific string. Used to seed-and-extend both read and its reverse-complement.
+
+        Return: result of extend()
+        """
+        seeds = self.find_seeds_for_one_read(w)
+        
+        l = []
+        for seed in seeds:
+            score = self.extend(w, seed)
+
+            l.append(score)
+
+        if len(l) > 0:
+            min_score_seed = min(l, key=lambda pos_score: pos_score[1])
+            return min_score_seed
+
+        return None
+
+
     def mapping(self):
+        """Map each read of the list in the reference
+
+        Return: dict{position in the reference : reads found at this position (list)}
+            Where each read found is the read or its reverse-complement
+        """
         result = {}
         for read in self.reads_lst:
-            seeds = self.find_seeds_for_one_read(read)
+            score_normal = self.align(read)
+            score_rev = self.align(utils.revcomp(read))
+
+            if score_normal is None:
+                best = score_rev
+            elif score_rev is None:
+                best = score_normal
+            else:
+                best = min(score_normal, score_rev, key=lambda pos_score: pos_score[1])
             
-            l = []
-            for seed in seeds:
-                score_normal = self.extend(read, seed)
-                score_revcomp = self.extend(utils.revcomp(read), seed)
+            if best is None: # Not found
+                continue
 
-                if score_normal is None:
-                    best = score_revcomp
-                elif score_revcomp is None:
-                    best = score_normal
-                elif score_normal[1] < score_revcomp[1]:
-                    best = score_normal
-                elif score_normal[1] > score_revcomp[1]:
-                    best = score_revcomp
+            pos = best[0]
+            if pos not in result:
+                result[pos] = []
 
-                if best is not None:
-                    l.append(best)
-
-            if len(l) > 0:
-                min_score_seed = min(l, key=lambda pos_score: pos_score[1])
-                #print(min_score_seed)
-                result[min_score_seed[0]] = read
+            if best == score_normal:
+                found_in_ref = read
+            else:
+                found_in_ref = utils.revcomp(read)
+            result[pos].append(found_in_ref)
 
         return result
