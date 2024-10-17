e6a470de39a37de743c0c8778bfe0a30a34d4a6a
Iziny <yoanndewilde@gmail.com>
2024-10-17 15:14:13 +0200

    Rename main to read_organizer

--- read_organizer.py
+++ read_organizer.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-i", "--input", required=True, help="input reads file")
 parser.add_argument("-r", "--reference", required=True, help="reference genome file")
 parser.add_argument("-k", type=int, help="seed size (only for seed-and-extend)")
-parser.add_argument("-m", "--method", choices=["seed", "fast"], required=True, help="mapping method: 'seed' for seed-and-extend, 'fast' for faster")
+parser.add_argument("-m", "--method", choices=["seed", "fast", "1", "2"], required=True, help="mapping method: 'seed' or 1 for seed-and-extend, 'fast' or 2 for faster")
 parser.add_argument("-o", "--output", required=True, help="output file")
 
 args = parser.parse_args()
@@ -22,7 +22,7 @@ reads = readfiles.read_reads(args.input)
 # print("REF=", ref)
 # print("READS=", reads)
 
-if args.method == "seed":
+if args.method in ["seed", "1"]:
     mapper = mapping.MappingBest(ref, reads, args.k)
 else:
     mapper = mapping.MappingFast(ref, reads, args.k)
